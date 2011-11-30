2c8c8ad4dbabc0ad6ad1c8069079b6f51621de66
angeloce <garcheck@gmail.com>
2011-11-30 22:27:17 +0800

    add sqlalchemy, mako, jquery

---
+++ debug.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
+
+import web
+
+urls = ('/.*', 'index')
+
+s = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>网络媒体技术部</title>
+    <script src="/static/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+</head>
+<body>
+<?php require_once 'inc/header.view.php'; ?>
+<div class="main">
+    <h1>B4网媒测试平台</h1>
+</div>
+<?php require_once 'inc/footer.view.php'; ?>
+</body>
+</html>
+"""
+
+class index:
+    def GET(self):
+        return s
+
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
