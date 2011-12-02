0f2dbb65f150f50c927ec5fc9dc412e7dd805462
angeloce <garcheck@gmail.com>
2011-12-02 22:25:50 +0800

    初始化基本的页面布局和样式

---
+++ views/__init__.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+import os
+from mako.lookup import TemplateLookup as _MakoTemplateLookup
+
+
+
+_FACTORY = _MakoTemplateLookup(docs=[os.path.dirname(__file__)])
+
+
+def Template(uri):
+    if not uri.endswith(".html"):
+        uri = uri + ".html"
+    return _FACTORY.get_template(uri)
